a3c5472d254ecd96bc678cd16a4fbbf4286ce4fd
iurypedrosa <iuryppedrosa@protonmail.com>
2024-04-23 16:08:22 -0300

    new exercise

---
+++ remote-control-competition/src/main/java/RemoteControlCar.java
@@ -0,0 +1,10 @@
+public interface RemoteControlCar {
+    // TODO implement the RemoteControlCar interface
+    void drive();
+
+    int getDistanceTravelled();
+
+    int getNumberOfVictories();
+
+    void setNumberOfVictories(int numberOfVictories);
+}
